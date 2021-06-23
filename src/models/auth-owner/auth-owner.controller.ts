4d410a91c8de11bd10b86dfc5756df0ed733dfaa
박상은 <peng1650@naver.com>
2021-06-24 02:13:49 +0900

    validate service create

--- src/models/auth-owner/auth-owner.controller.ts
+++ src/models/auth-owner/auth-owner.controller.ts
@@ -4,4 +4,13 @@ import { AuthOwnerService } from './auth-owner.service';
 @Controller('auth-owner')
 export class AuthOwnerController {
   constructor(private readonly authOwnerService: AuthOwnerService) {}
+
+  async validateUser(ownername: string, psswd: string): Promise<any> {
+    const owner = await this.authOwnerService.findOne(ownername);
+    if (owner && owner.psswd === psswd) {
+      const { password, ...result } = owner;
+      return result;
+    }
+    return null;
+  }
 }
