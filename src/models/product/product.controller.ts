5cdc0a7c6858eda0ff0ca878b8fa1a73b91d72aa
harrysci <59914405+harrysci@users.noreply.github.com>
2021-06-23 17:10:11 +0900

    엑셀 파일 패키지 만들기

--- src/models/product/product.controller.ts
+++ src/models/product/product.controller.ts
@@ -1,7 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { ProductService } from './product.service';
-
+import * as XLSX from 'xlsx';
 @Controller('product')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
+
+  @Post()
+  @UseInterceptors(FileInterceptor('file'))
+  async handleExcel(@UploadedFile() file){
+    const workbook= XLSX.read(file.buffer, {type: 'buffer'});
+    const sheetName= workbook.SheetNames[0];
+
+    const sheet= workbook.Sheets[sheetName];
+
+    const rows= XLSX.utils.sheet_to_json(sheet, {
+      defval:null,
+    });
+    
+  }
+
 }
