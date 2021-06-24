9d0aad88a62b36c887031ddb43cf506bc9e0c3c3
KIMBEOBWOO <qjqdn1568@naver.com>
2021-06-24 17:13:47 +0900

    주석 처리

--- src/models/product/product.service.ts
+++ src/models/product/product.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { GetProductListRes } from './dto/getProductListRes.dto';
 import { UpdateProductInfoReq } from './dto/updateProductInfoReq.dto';
+import { UpdateProductInfoRes } from './dto/updateProductInfoRes.dto';
 import { OnsaleProduct } from './entities/onsale-product.entity';
 import { ProcessedProduct } from './entities/processed-product.entity';
 import { Product } from './entities/product.entity';
@@ -27,6 +28,12 @@ export class ProductService {
   /**********************************************************************************
    * @점주WebApp
    **********************************************************************************/
+
+  /**
+   * 점주 및 점포 관리인 웹 대시보드 상품 목록 조회
+   * @param storeId 가게 id
+   * @returns GetProductListRes[], 웹 요청 상품 정보 리스트 반환
+   */
   async getProductList(storeId: number) {
     /* Database 조회 결과 */
     const selectProductListRawResult = await this.productRepository
@@ -59,8 +66,16 @@ export class ProductService {
     return productList;
   }
 
-  async updateProductInfo(updateProductInfo: UpdateProductInfoReq) {
-    const selectProductRawResult = await this.productRepository
+  /**
+   * 점주 및 점포 관리인 웹 대시보드 개별 상품 정보 수정
+   * @param updateProductInfo 수정할 상품의 id 및 수정할 상품 정보
+   * @returns UpdateProductInfoRes , 수정된 웹 요청 상품 정보 반환
+   */
+  async updateProductInfo(
+    updateProductInfo: UpdateProductInfoReq,
+  ): Promise<UpdateProductInfoRes> {
+    /* product id 를 통해 원본 데이터 조회 */
+    const selectProductRawResult: Product = await this.productRepository
       .createQueryBuilder('product')
       .where('product.product_id = :product_id', {
         product_id: updateProductInfo.productId,
@@ -70,6 +85,11 @@ export class ProductService {
       .leftJoinAndSelect('product.weighted_product', 'weighted_product')
       .getOne();
 
+    /**
+     * 1. on sale product 정보 수정
+     * 2. processed product 정보 수정
+     * 3. weighted product 정보 수정
+     */
     selectProductRawResult.onsale_product = {
       ...selectProductRawResult.onsale_product,
       product_onsale_price: updateProductInfo.productOnSalePrice
@@ -88,8 +108,10 @@ export class ProductService {
         weighted_product_volume: updateProductInfo.productVolume,
       };
     }
+    /* 수정한 정보 적용 */
     await this.productRepository.save(selectProductRawResult);
 
+    /* product 정보 수정 */
     await this.productRepository
       .createQueryBuilder('prdouct')
       .update(Product)
@@ -106,7 +128,8 @@ export class ProductService {
       })
       .execute();
 
-    const selectUpdatedProductRawResult = await this.productRepository
+    /* 수정된 DB 정보 조회 */
+    const selectUpdatedProductRawResult: Product = await this.productRepository
       .createQueryBuilder('product')
       .where('product.product_id = :product_id', {
         product_id: updateProductInfo.productId,
@@ -116,7 +139,8 @@ export class ProductService {
       .innerJoinAndSelect('product.processed_product', 'processed_product')
       .getOne();
 
-    const updatedProductInfo = {
+    /* 점주 및 점포 관리인 상품 정보 수정 요청 결과로 변환 */
+    const updatedProductInfo: UpdateProductInfoRes = {
       productId: selectUpdatedProductRawResult.product_id,
       productBarcode: selectUpdatedProductRawResult.product_barcode,
       productName: selectUpdatedProductRawResult.product_name,
@@ -138,8 +162,14 @@ export class ProductService {
     return updatedProductInfo;
   }
 
+  /**
+   * 점주 및 점포 관리인 웹 대시보드 개별 상품 삭제 (on sale, processed, weighted 동시 삭제)
+   * @param productId 삭제할 상품의 id
+   * @returns void
+   */
   async deleteProductInfo(productId: number): Promise<void> {
-    const selectTargetProductRawResult = await this.productRepository
+    /* product id 를 통해 원본 데이터 조회 */
+    const selectTargetProductRawResult: Product = await this.productRepository
       .createQueryBuilder('product')
       .where('product.product_id = :product_id', { product_id: productId })
       .leftJoinAndSelect('product.onsale_product', 'onsale_product')
@@ -147,8 +177,10 @@ export class ProductService {
       .leftJoinAndSelect('product.weighted_product', 'weighted_product')
       .getOne();
 
+    /* 1. foreign key 를 가진 해당 product 삭제 */
     await this.productRepository.remove(selectTargetProductRawResult);
 
+    /* 2. foreign key 의 대상 relation table raw 삭제 */
     if (selectTargetProductRawResult.onsale_product) {
       const target = await this.onSaleProductRepository.findOne({
         onsale_product_id:
@@ -170,7 +202,5 @@ export class ProductService {
       });
       await this.weightedProductRepository.remove(target);
     }
-
-    return;
   }
 }
