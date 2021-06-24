ac5960221fb0b32a44ba9689d5861ef57f1a3ad2
박상은 <sangeun@bagsang-eun-ui-MacBookPro.local>
2021-06-24 21:12:30 +0900

    create owner authOwnerService, jwtStrategy add login router to app.module

--- src/models/auth-owner/auth-owner.controller.ts
+++ src/models/auth-owner/auth-owner.controller.ts
@@ -1,16 +1,8 @@
 import { Controller } from '@nestjs/common';
 import { AuthOwnerService } from './auth-owner.service';
+// import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth-owner')
 export class AuthOwnerController {
   constructor(private readonly authOwnerService: AuthOwnerService) {}
-
-  async validateUser(ownername: string, psswd: string): Promise<any> {
-    const owner = await this.authOwnerService.findOne(ownername);
-    if (owner && owner.psswd === psswd) {
-      const { password, ...result } = owner;
-      return result;
-    }
-    return null;
-  }
 }
--- src/models/auth-owner/auth-owner.module.ts
+++ src/models/auth-owner/auth-owner.module.ts
@@ -1,13 +1,22 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AuthOwnerController } from './auth-owner.controller';
 import { AuthOwnerService } from './auth-owner.service';
 import { Owner } from './entities/owner.entity';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './constants';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Owner])],
-  exports: [AuthOwnerService],
-  providers: [AuthOwnerService],
-  controllers: [AuthOwnerController],
+  imports: [
+    TypeOrmModule.forFeature([Owner]),
+    PassportModule,
+    JwtModule.register({
+      secret: jwtConstants.secrete,
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
+  exports: [AuthOwnerService, JwtModule],
+  providers: [AuthOwnerService, JwtStrategy],
 })
 export class AuthOwnerModule {}
