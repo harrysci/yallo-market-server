cf38ef03f4d7d1ed8aa084ae77c6870ee25b7c90
박상은 <sangeun@bagsang-eun-ui-MacBookPro.local>
2021-09-09 21:42:36 +0900

    order parent entity fix

--- src/models/order/order.service.ts
+++ src/models/order/order.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { Store } from '../store/entities/store.entity';
 import { OrderChildGet } from './dto/OrderChildGet.dto';
 import { OrderParentGet } from './dto/OrderParentGet.dto';
 import { OrderChild } from './entities/order-child.entity';
